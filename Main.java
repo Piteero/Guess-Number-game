d1c1adb2f5da7b8e6bfb1ce10ee75ce25b7d9602
Piotr <piteeroo@yahoo.com>
2016-01-27 22:52:28 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+package com.GuessNumber.Launching;
+
+import com.GuessNumber.Calculation.Calculation;
+
+public class Main
+{
+
+	public static void main(String[] args)
+	{
+		Calculation calculation = new Calculation();
+		calculation.runProgram();
+	}
+
+}
